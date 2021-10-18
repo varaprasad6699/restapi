cda7de5080d28fe3ae20c969cf96c161027e66db
varaprasad <19pa1a0533@vishnu.edu.in>
2021-10-18 20:44:03 +0530

    3rd  commit

--- restapi/urls.py
+++ restapi/urls.py
@@ -32,8 +32,6 @@ router.register('api',EmployeeAll,basename="employee")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls)),
-     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
  
