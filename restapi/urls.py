b933be600a20367e3f7acba057fe77a95e8e6314
varaprasad <19pa1a0533@vishnu.edu.in>
2021-10-18 20:36:29 +0530

    host  commit

--- restapi/urls.py
+++ restapi/urls.py
@@ -20,7 +20,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from api.views import EmployeeAll
+from django.views.static import serve
+from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 router=DefaultRouter()
 router.register('api',EmployeeAll,basename="employee")
@@ -28,6 +32,8 @@ router.register('api',EmployeeAll,basename="employee")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls)),
+     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
  
